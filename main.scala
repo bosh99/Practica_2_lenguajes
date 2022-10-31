75ae580f9d98fcd7da547ab78bb373398eb74c1e
bosh99 <bosh99@github.com>
2022-10-31 07:52:32 -0500

    Commit final

--- main.scala
+++ main.scala
@@ -1,63 +1,58 @@
 import scala.io.StdIn.readLine
 
-sealed trait Tree[+A] 
-case object Nil extends Tree[Nothing]
-case class Leaf[+A](value: A) extends Tree[A] //caso hoja
-case class Branch[+A](left:Tree[A],operator:A,right:Tree[A]) extends Tree[A] //caso rama --> nodo con hijos 
+sealed trait Tree
+case object Nil extends Tree
+case class Leaf(value: Char) extends Tree
+case class Branch(left:Tree,operator:Char,right:Tree) extends Tree 
 
 
 object Tree {
 
-    def apply[A](expression:Array[A]): Tree[A] = {
-
-        def go[A](i: Int, operators:List[String], expression:Array[A]): (Tree[A], Int) = {
-            if (expression.isEmpty) (Nil,0)
-            else {
-                val bool = operators.contains(expression(i))
-                if (bool) {
-                    val left = go(i+1, operators, expression)
-                    val j = left._2
-                    val right = go(j+1, operators, expression)
-                    (Branch(left._1, expression(i), right._1),j+1)
-                } 
-                else (Leaf(expression(i)), i)
-            }
-        }
-        go(0, List("*","+","-","/"), expression)._1
+    def apply(expression: List[Char]): Tree = {
+        buildTree(expression, List('*','+','-','/'))._1
     }
 
-    def translate[A](tree: Tree[A]): String = tree match {
+    def buildTree(expression: List[Char], operators:List[Char]): (Tree, List[Char]) = expression match {
+        case e if (e.isEmpty) => (Nil, List())
+        case head :: tail if (!operators.contains(head)) => (Leaf(head), tail)
+        case head :: tail if (operators.contains(head)) => {
+            val (left, restAfterLeft) = buildTree(tail, operators)
+            val (right, restAfterRight) = buildTree(restAfterLeft, operators)
+            (Branch(left, head, right), restAfterRight)
+        }
+        case _ => sys.error("There was a problem...")
+    }
+
+    def translate(tree: Tree): String = tree match {
         case Nil => ""
         case Leaf(value) => value.toString()
         case Branch(left, operator, right) => {
             val x = translate(left)
             val y = translate(right)
-            val result = x + operator.toString() + y
+            val result = x + operator + y
             result
         }
     }
 
-    def calculate[A](tree: Tree[A]): Int = tree match {
+    def calculate(tree: Tree): Int = tree match {
         case Nil => 0
         case Leaf(value) => value.toString().toInt
         case Branch(left, operator, right) => {
             val x = calculate(left)
             val y = calculate(right)
-            val result = operate(x, y, operator.toString())
-            result
+            val result = operate(x, y, operator)
+            result.getOrElse(0)
         }
     }
 
-    def operate(num1: Int, num2: Int, operation: String):Int = operation match {
-        case "+" => num1+num2
-        case "*" => num1*num2
-        case "-" => num1-num2
-        case "/" => {
-            try {
-                num1/num2
-            } catch {
-                case cero: ArithmeticException => sys.error("Sorry there was a problem...")
-            }
+    def operate(num1: Int, num2: Int, operation: Char):Option[Int] = operation match {
+        case '+' => Some(num1+num2)
+        case '*' => Some(num1*num2)
+        case '-' => Some(num1-num2)
+        case '/' => try {
+            Some(num1/num2)
+        } catch {
+            case e: ArithmeticException => println("I can`t divide by 0"); sys.exit(0)
         }
     }
 
@@ -68,18 +63,20 @@ object Main{
     def main(args: Array[String]): Unit = {
         print("Please enter the operation (separated by spaces): ")
         val expression = readLine()
-        val expOnTree = Tree(expression.split(" "))
+
+        val expOnTree = Tree(expression.toList)
         println(expOnTree)
 
         val algebraicExpression = Tree.translate(expOnTree)
         println(s"Algebraic expression: $algebraicExpression")
+
         val result = Tree.calculate(expOnTree)
         println(s"Result: $result")
 
 
     }
 
-    // * + 5 9 - 7 8
-    // * - 5 7 / 4 0
-    // / * - 4 2 + 7 1 - * 2 2 + 9 1
+    // *+59-78
+    // *-57/40
+    // /*-42+71-*22+91
 }
